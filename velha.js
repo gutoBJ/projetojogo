462d4548f63d73825d4059ba3ac76e245718ce87
unknown <suporte@uept46-5402-e.aluno.pr.senac.br>
2025-06-23 18:59:26 -0300

    jogo da velha

--- velha.js
+++ velha.js
@@ -1,21 +1,38 @@
-let x_o1 = document.getElementById('x_o1')
-let x_o2 = document.getElementById('x_o2')
-let x_o3 = document.getElementById('x_o3')
-let x_o4 = document.getElementById('x_o4')
-let x_o5 = document.getElementById('x_o5')
-let x_o6 = document.getElementById('x_o6')
-let x_o7 = document.getElementById('x_o7')
-let x_o8 = document.getElementById('x_o8')
-let x_o9 = document.getElementById('x_o9')
+// let x_o1 = document.getElementById('x_o1')
+// let x_o2 = document.getElementById('x_o2')
+// let x_o3 = document.getElementById('x_o3')
+// let x_o4 = document.getElementById('x_o4')
+// let x_o5 = document.getElementById('x_o5')
+// let x_o6 = document.getElementById('x_o6')
+// let x_o7 = document.getElementById('x_o7')
+// let x_o8 = document.getElementById('x_o8')
+// let x_o9 = document.getElementById('x_o9')
 
-let x = 'x'
-let o = 'o'
+const botoesVelha = document.querySelectorAll('.botao-velha')
 
-const l1 = [x_o1, x_o2, x_o3]
-const l2 = [x_o4, x_o5, x_o6]
-const l3 = [x_o7, x_o8, x_o9]
+let contador = 0
 
-const velha = [l1, l2, l3]
+console.log(botoesVelha)
+
+for (let i = 0; i <= botoesVelha.length; i++) {
+    botoesVelha[i].addEventListener('click', () => {
+        if (contador % 2 === 0) {
+            botoesVelha[i].textContent = 'X'
+            contador++
+        } else {
+            botoesVelha[i].textContent = 'O'
+            contador = 0
+        }
+    })
+}
+
+
+
+// const l1 = [x_o1, x_o2, x_o3]
+// const l2 = [x_o4, x_o5, x_o6]
+// const l3 = [x_o7, x_o8, x_o9]
+
+// const velha = [l1, l2, l3]
 
 function venceu(player) {
     
